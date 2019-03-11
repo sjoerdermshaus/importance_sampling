528afab18dfe00ccdc066be076e2393d42b08277
sjoerdermshaus <sjoerdermshaus@gmail.com>
2019-03-11 20:14:26 +0100

    Left tail and right tail percentile tests added in combination with IS

--- test_importanceSampling.py
+++ test_importanceSampling.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import numpy as np
 from main import ImportanceSampling as ImSa
+from scipy.stats import norm
 
 
 class TestImportanceSampling(TestCase):
@@ -39,6 +40,26 @@ class TestImportanceSampling(TestCase):
     def test_percentile_regular_50(self):
         for likelihood_ratio in self.likelihood_ratios:
             self.assertEqual(ImSa.percentile(self.data, 50, likelihood_ratio), 3)
+
+    def test_percentile_IS_left_tail(self):
+        shift = -2
+        shifted_data = self.data + shift
+        p = norm.pdf(shifted_data, loc=3.5, scale=1)
+        q = norm.pdf(shifted_data, loc=1.5, scale=1)
+        likelihood_ratio = p / q
+        results = [[0.01, -1], [0.1, 0], [0.14, 0], [1.1, 1], [7, 2], [49, 3], [50, 3]]
+        for r in results:
+            self.assertEqual(ImSa.percentile(shifted_data, r[0], likelihood_ratio), r[1])
+
+    def test_percentile_IS_right_tail(self):
+        shift = 3
+        shifted_data = self.data + shift
+        p = norm.pdf(shifted_data, loc=3.5, scale=1)
+        q = norm.pdf(shifted_data, loc=6.5, scale=1)
+        likelihood_ratio = p / q
+        results = [[99.999, 9], [99.998, 8], [99.997, 8], [99.96, 7], [98, 6], [60, 5]]
+        for r in results:
+            self.assertEqual(ImSa.percentile(shifted_data, r[0], likelihood_ratio), r[1])
 
     def tearDown(self):
         pass
