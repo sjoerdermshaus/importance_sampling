78e4794d940e1df68541a4e64bbd8e49232e180d
sjoerdermshaus <sjoerdermshaus@gmail.com>
2019-03-20 19:32:24 +0100

    Restructuring of code

--- main.py
+++ main.py
@@ -17,20 +17,17 @@ def define_run():
     shifts = np.linspace(0, 6, 13)
     shifts = np.sort(np.append(shifts, norm.ppf(quantile / 100.0)))
     sim_sizes = 10
-    args = dict(quantile=quantile,
-                sample_sizes=sample_sizes,
-                shifts=shifts,
-                sim_sizes=sim_sizes,
-                pool_size=10)
-    return args
+    kwargs = dict(quantile=quantile,
+                  sample_sizes=sample_sizes,
+                  shifts=shifts,
+                  sim_sizes=sim_sizes,
+                  pool_size=10)
+    return kwargs
 
 
 if __name__ == '__main__':
-    preload = True
+    preload = False
     my_args = define_run()
-    if preload:
-        my_df = pd.read_excel('results.xlsx')
-    else:
-        my_df = ImportanceSampling(**my_args).run()
+    my_df = pd.read_excel('results.xlsx') if preload else ImportanceSampling(**my_args).run()
     my_fig = create_plot(args=my_args, df=my_df, figsize=12, dpi=500)
     plt.show(my_fig)
